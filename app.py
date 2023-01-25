2fabe02e73569f4b3326425162041f45ad018fb1
rafaelrodrix <rafaelrodrix@gmail.com>
2023-01-25 10:14:01 -0300

    commit alteração na função app.get

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
-@app.get('/')
-async def index():
-    return 'Está funcionando'
+@app.get("/")
+async def root():
+    with sqlite3.connect('db.db') as conn:
+        cur = conn.cursor()
+
+        query = 'select * from entidades'
+
+        cur.execute(query)
+
+        data = cur.fetchall()
+        print (data)
+    return {"message": "Está Funcionando"}
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='0.0.0.0', port=7777)
+    uvicorn.run(app=app, host='localhost', port=5555)
